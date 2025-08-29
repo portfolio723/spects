137ea64f10e9c19fc7144cd51f7c10b43fa1ab4f
Sandeep M <sandeep723ms@gmail.com>
2025-08-29 07:03:11 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/shastra-spectacles' : undefined,
   typescript: {
     ignoreBuildErrors: true,
   },
